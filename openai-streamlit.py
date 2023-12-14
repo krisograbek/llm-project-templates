51c3a09a886acc91521290c7683d907fb551937f
krisograbek <krzysztof.ograbek@gmail.com>
2023-12-14 08:12:26 +0100

    prepare the Streamlit app, add license

--- openai-streamlit.py
+++ openai-streamlit.py
@@ -1,5 +1,6 @@
 from openai import OpenAI
 from dotenv import load_dotenv
+import streamlit as st
 
 # Load API Keys
 load_dotenv()
@@ -7,16 +8,36 @@ load_dotenv()
 # initialize OpenAI API
 client = OpenAI()
 
-# Set a specific role, behavior, tone, etc.
-# System message is active throughout the whole converstation
-system_prompt = """You are a helpful assistant speaking Pirate English"""
+# define the model you want to use
+model = "gpt-4-1106-preview"
 
-completion = client.chat.completions.create(
-    model="gpt-4-1106-preview",
-    messages=[
-        {"role": "system", "content": system_prompt},
-        {"role": "user", "content": "How to learn fast?"},
-    ],
-)
+st.title("My Own ChatGPT!🤖")
 
-print(completion.choices[0].message.content)
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state["messages"]:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if "model" not in st.session_state:
+    st.session_state.model = model
+
+if user_prompt := st.chat_input("Your prompt"):
+    st.session_state.messages.append({"role": "user", "content": user_prompt})
+    with st.chat_message("user"):
+        st.markdown(user_prompt)
+
+    # generate responses
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        response = client.chat.completions.create(
+            model=st.session_state.model,
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+        )
+        message_placeholder.markdown(response.choices[0].message.content)
+
+    st.session_state.messages.append({"role": "assistant", "content": response})
